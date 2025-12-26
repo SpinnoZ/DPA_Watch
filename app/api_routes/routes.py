ce391e8cacbc06ad1029b50c477319aada7d0772
Win11_Secu <spinnlex@gmail.com>
2025-12-26 14:51:31 +0300

    solved signed_date issue, return partner name by ID, added single contract api request

--- app/api_routes/routes.py
+++ app/api_routes/routes.py
@@ -7,33 +7,62 @@ from flask import jsonify
 
 from app.api_routes import api_bp
 
+def contracts_to_dict(contracts):
+       contract_dict = [
+            {
+                "contractID": c.contract_id,
+                "registrationDate" : c.registration_date.isoformat() if isinstance(c.registration_date, date) else None,
+                "contractTitle" : c.contract_title,
+                "contractDescription" : c.contract_description,
+                "contractNo": c.contract_no,
+                "contractForm": "DPA" if c.contract_form else "DSA",
+                "contractStatus" : c.contract_status,
+                "partnerName" : c.partner.partner_name if c.partner else None, #replace to a query to a partner's name
+                "signed_date": c.signed_date.isoformat() if c.signed_date else None,
+                "systemRegistered": "yes" if c.system_registered else "no",
+                "reportedHQ": "yes" if c.HQ_reported else "no",
+                "picID": c.PIC_id,
+                "departmentID": c.PIC_team
+            }
+            for c in contracts
+        ]
+       return {"contract_list":contract_dict}
+
+
 @api_bp.route('/api_contract_list/', methods = ['GET'])
 def api_contract_list():
     stmt = select(Contract).limit(5)
     contracts = db.session.scalars(stmt).all()
 
     # building contract dicts for json
-    contract_dicts = [
-        {
-            "contractID": c.contract_id,
-            "registrationDate" : c.registration_date.strftime("%Y-%m-%d"),
-            "contractTitle" : c.contract_title,
-            "contractDescription" : c.contract_description,
-            "contractNo": c.contract_no,
-            "contractForm": "DPA" if c.contract_form else "DSA",
-            "contractStatus" : c.contract_status,
-            "partnerID" : "partnerID", #replace to a query to a partner's name
-            "signed_date": c.signed_date.strftime("%Y-%m-%d"),
-            "systemRegistered": "yes" if c.system_registered else "no",
-            "reportedHQ": "yes" if c.HQ_reported else "no",
-            "picID": c.PIC_id,
-            "departmentID": c.PIC_team
-        }
-        for c in contracts
-    ]
+    # contract_dicts = [
+    #     {
+    #         "contractID": c.contract_id,
+    #         "registrationDate" : c.registration_date.isoformat() if isinstance(c.registration_date, date) else None,
+    #         "contractTitle" : c.contract_title,
+    #         "contractDescription" : c.contract_description,
+    #         "contractNo": c.contract_no,
+    #         "contractForm": "DPA" if c.contract_form else "DSA",
+    #         "contractStatus" : c.contract_status,
+    #         "partnerName" : c.partner.partner_name if c.partner else None, #replace to a query to a partner's name
+    #         "signed_date": c.signed_date.isoformat() if c.signed_date else None,
+    #         "systemRegistered": "yes" if c.system_registered else "no",
+    #         "reportedHQ": "yes" if c.HQ_reported else "no",
+    #         "picID": c.PIC_id,
+    #         "departmentID": c.PIC_team
+    #     }
+    #     for c in contracts
+    # ]
+    contract_dicts = contracts_to_dict(contracts)
 
     # Wrap the dict structure
-    response = { "contract_list": contract_dicts}
+    # response = { "contract_list": contract_dicts}
 
 
-    return jsonify(response), 200
+    return jsonify(contract_dicts), 200
+
+@api_bp.route('/api_contract/<int:contractID>', methods = ['GET'])
+def api_contract(contractID):
+    stmt = select(Contract).where(Contract.contract_id == contractID)
+    contracts_query = db.session.scalar(stmt)
+    return jsonify(contracts_to_dict([contracts_query])), 200
